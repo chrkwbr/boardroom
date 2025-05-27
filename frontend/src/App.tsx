b6366b70df61993abe273562bad9920453ad757e
chiharu-kuwabara <chrkwbr@gmail.com>
2025-05-28 02:47:54 +0900

    fix: refactor App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,28 @@
 import "./App.css";
-import MainPanel from "./components/MainPanel.tsx";
+import Header from "./components/header/Header.tsx";
+import Sidebar from "./components/room/Sidebar.tsx";
+import Content from "./components/content/Content.tsx";
+import {Route, Routes} from "react-router-dom";
 
 const App = () => {
   return (
-    <div data-theme="">
-      <MainPanel />
+    <div className="flex flex-col h-screen" data-theme="">
+      <div className="flex-none">
+        <Header />
+      </div>
+      <div className="flex flex-1 overflow-y-auto">
+        <div className="flex sm:flex-col md:flex-row w-full">
+          <Sidebar />
+          <div className="px-1 flex-grow flex-shrink">
+            <Routes>
+              <Route
+                path="/:roomId"
+                element={<Content />}
+              />
+            </Routes>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
